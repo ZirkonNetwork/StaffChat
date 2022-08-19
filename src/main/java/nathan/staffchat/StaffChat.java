5e69e71d7623ac5b6a2f0eb3e2e887bad203a06a
Elephant1214 <nathangetsmail@gmail.com>
2022-08-18 19:57:19 -0600

    Remove extra line

--- src/main/java/nathan/staffchat/StaffChat.java
+++ src/main/java/nathan/staffchat/StaffChat.java
@@ -13,7 +13,6 @@ import java.util.Objects;
 public final class StaffChat extends JavaPlugin {
 
     public static final String VERSION = "1.2.0";
-
     public static JavaPlugin plugin = null;
     private static String contentSeparator = null;
     public static String pluginPrefix = null;
